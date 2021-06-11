7afdea94a4cb39b24e5f4d1ff851b4690976d8ca
plat67 <good2060@naver.com>
2021-06-11 21:33:55 +0900

    Feat: GPS

--- app/src/main/java/com/android/study/hanselandphotograph/ui/RecordingStoryActivity.kt
+++ app/src/main/java/com/android/study/hanselandphotograph/ui/RecordingStoryActivity.kt
@@ -1,23 +1,54 @@
 package com.android.study.hanselandphotograph.ui
 
 import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.icu.text.SimpleDateFormat
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.study.hanselandphotograph.R
+import com.android.study.hanselandphotograph.adapter.PicListAdapter
 import com.android.study.hanselandphotograph.databinding.ActivityRecordingStoryBinding
+import com.android.study.hanselandphotograph.model.Picture
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
 import java.io.File
 
 class RecordingStoryActivity : AppCompatActivity() {
     lateinit var binding: ActivityRecordingStoryBinding
+    lateinit var adapter: PicListAdapter
+    var picList: ArrayList<Picture> = ArrayList()
+
+    lateinit var googleMap: GoogleMap
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
+    var startUpdate = false
+    var loc = LatLng(37.554752, 126.970631)
+    val arrLoc = ArrayList<LatLng>()
+
     val PERMISSIONS = arrayOf(
         Manifest.permission.CAMERA,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -26,13 +57,36 @@ class RecordingStoryActivity : AppCompatActivity() {
     val PERMISSIONS_REQUEST = 100
     private val BUTTON = 100
     private var photoUri: Uri? = null
-    lateinit var filepath:String
+    lateinit var filepath: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecordingStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initToolbar()
+        initRecyclerView()
+        initMap()
         init()
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        adapter = PicListAdapter(picList)
+        adapter.itemClickListener = object : PicListAdapter.OnItemClickListener {
+            override fun onItemClick(
+                holder: PicListAdapter.ViewHolder,
+                view: View,
+                data: Picture,
+                position: Int
+            ) {
+                val intent = Intent(this@RecordingStoryActivity, ShowImageActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        binding.recyclerView.adapter = adapter
+
     }
 
     private fun initToolbar() {
@@ -44,10 +98,179 @@ class RecordingStoryActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_new_24)
     }
 
+    private fun initMap() {
+        initLocation()
+
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync {
+            googleMap = it
+            initMapListener()
+        }
+    }
+
+    private fun initLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(location: LocationResult) {
+                if (location.locations.size == 0) return
+                loc = LatLng(
+                    location.locations[location.locations.size - 1].latitude,
+                    location.locations[location.locations.size - 1].longitude
+                )
+                setCurrentLocation(loc)
+                Log.i("location", "LocationCallback()")
+            }
+        }
+    }
+
+    private fun initMapListener() {
+        googleMap.setOnMapClickListener {
+            // distance check
+
+            arrLoc.add(it)
+            googleMap.clear()
+
+            val option = MarkerOptions()
+            option.position(it)
+            option.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+            googleMap.addMarker(option)
+
+            /*val option2 = PolylineOptions().color(Color.GREEN).addAll(arrLoc)
+            googleMap.addPolyline(option2)*/
+
+            val option2 =
+                PolygonOptions().fillColor(Color.argb(100, 255, 255, 0))
+                    .strokeColor(Color.GREEN)
+                    .addAll(arrLoc)
+            googleMap.addPolygon(option2)
+        }
+    }
+
+    fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                ), 100
+            )
+        } else {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                if (it != null) {
+                    loc = LatLng(it.latitude, it.longitude)
+                    setCurrentLocation(loc)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+                // getLastLocation()
+            } else {
+                Toast.makeText(this, "위치 정보가 필요합니다.", Toast.LENGTH_SHORT).show()
+                setCurrentLocation(loc)
+            }
+        }
+    }
+
+    fun setCurrentLocation(location: LatLng) {
+        val option = MarkerOptions()
+        option.position(location)
+        option.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        googleMap.addMarker(option)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16.0f))
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                ), 100
+            )
+        } else {
+            if (!checkLocationServicesStatus()) { // GPS가 켜져 있는지 확인
+                showLocationServicesSetting()
+            } else {
+                startUpdate = true
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest, locationCallback, Looper.getMainLooper()
+                )
+                Log.i("loaction", "startLocationUpdate()")
+            }
+        }
+    }
+
+    private fun stopLocationUpdate() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        startUpdate = false
+        Log.i("location", "stopLocationUpdate()")
+    }
+
+    private fun checkLocationServicesStatus(): Boolean {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+
+    }
+
+    private fun showLocationServicesSetting() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n" + "위치 설정을 허용하겠습니까?")
+        builder.setPositiveButton("설정", DialogInterface.OnClickListener { dialog, id ->
+            val gpsSettionIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivityForResult(gpsSettionIntent, 1000)
+        })
+        builder.setNegativeButton("취소", DialogInterface.OnClickListener { dialog, id ->
+            dialog.dismiss()
+            setCurrentLocation(loc)
+        })
+        builder.create().show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            1000 -> {
+                if (checkLocationServicesStatus()) {
+                    Toast.makeText(this, "GPS가 활성화 되었음", Toast.LENGTH_SHORT).show()
+                    startLocationUpdates()
+                }
+            }
+        }
+    }
+
     private fun init() {
         binding.apply {
             finishRecordBtn.setOnClickListener {
-                val intent = Intent(this@RecordingStoryActivity, CommentStoryActivity::class.java)
+                val intent =
+                    Intent(this@RecordingStoryActivity, CommentStoryActivity::class.java)
                 startActivity(intent)
             }
 
@@ -56,8 +279,8 @@ class RecordingStoryActivity : AppCompatActivity() {
                 checkPermissions(PERMISSIONS, PERMISSIONS_REQUEST)
                 val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                 val photoFile = File(
-                    File("${filesDir}/image").apply{
-                        if(!this.exists()){
+                    File("${filesDir}/image").apply {
+                        if (!this.exists()) {
                             this.mkdirs()
                         }
                     },
@@ -68,7 +291,8 @@ class RecordingStoryActivity : AppCompatActivity() {
                     "com.android.study.hanselandphotograph.fileprovider",
                     photoFile
                 )
-                takePictureIntent.resolveActivity(packageManager)?.also{
+
+                takePictureIntent.resolveActivity(packageManager)?.also {
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
                     startActivityForResult(takePictureIntent, BUTTON)
                 }
@@ -77,29 +301,34 @@ class RecordingStoryActivity : AppCompatActivity() {
     }
 
     private fun checkPermissions(permissions: Array<String>, permissionsRequest: Int): Boolean {
-        val permissionList : MutableList<String> = mutableListOf()
-        for(permission in permissions){
+        val permissionList: MutableList<String> = mutableListOf()
+        for (permission in permissions) {
             val result = ContextCompat.checkSelfPermission(this, permission)
-            if(result != PackageManager.PERMISSION_GRANTED){
+            if (result != PackageManager.PERMISSION_GRANTED) {
                 permissionList.add(permission)
             }
         }
-        if(permissionList.isNotEmpty()){
-            ActivityCompat.requestPermissions(this, permissionList.toTypedArray(), permissionsRequest)
+        if (permissionList.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionList.toTypedArray(),
+                permissionsRequest
+            )
             return false
         }
         return true
     }
 
-    private fun newJpgFileName() : String {
+    private fun newJpgFileName(): String {
         val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
         val filename = sdf.format(System.currentTimeMillis())
-        filepath = "data/data/com.android.study.hanselandphotograph/files/image/"+"${filename}.jpg"
+        filepath =
+            "data/data/com.android.study.hanselandphotograph/files/image/" + "${filename}.jpg"
         return "${filename}.jpg"
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             android.R.id.home -> {
                 onBackPressed()
             }
